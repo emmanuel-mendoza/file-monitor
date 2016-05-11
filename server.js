b5bbfca2515bae3b97a45bff21ee975513c381b6
Emmanuel Mendoza <emmanuel.mendoza@4thsource.com>
2016-05-11 10:49:26 -0500

    massive examples

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
+var config  = require('./server/config/config.json')
 var express = require('express');
+var massive = require('massive');
+
 var app = express();
+var connStr = "postgres://"+config.postgres.user+":"+config.postgres.password+"@"+config.postgres.host+"/"+config.postgres.db;
+
+console.log(connStr);
+
+var db = massive.connectSync({connectionString : connStr, scripts: "server/db"});
+
+db.appdata.users.find(2, function(err, res){
+    console.log(res);
+});
+
+db.find_user_by_email(['emmanuel.mendoza.sanchez@gmail.com'], function(err,res){
+  console.log(res);
+});
+
+db.run("select * from appdata.files", function(err,res) {
+    console.log(res);
+})
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
---
+++ server/db/find_user_by_email.sql
@@ -0,0 +1,1 @@
+select * from appdata.users where email = $1
